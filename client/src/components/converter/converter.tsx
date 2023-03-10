dae98a31585f223951b30df40f4541b629ff83d4
Jakub Valenta <jakub.valenta@rohlik.cz>
2023-03-10 16:01:24 +0100

    App error message

--- client/src/components/converter/converter.tsx
+++ client/src/components/converter/converter.tsx
@@ -1,30 +1,28 @@
-import moment from "moment";
-import { useExchangeRatesQuery } from "../../queries/exchangeRatesQuery";
-import { useTranslation } from "react-i18next";
-import { Card, CardContent, Container, Typography } from "@mui/material";
-import { ExchangeRates } from "../exchangeRates/exchangeRates";
-import { Form } from "../form/form";
-import { useState } from "react";
-import { Conversion } from "../conversion/conversion";
-import { ConversionData } from "../../types";
+import moment from 'moment';
+import { useExchangeRatesQuery } from '../../queries/exchangeRatesQuery';
+import { useTranslation } from 'react-i18next';
+import { Alert, Card, CardContent, Container, Typography } from '@mui/material';
+import { ExchangeRates } from '../exchangeRates/exchangeRates';
+import { Form } from '../form/form';
+import { useState } from 'react';
+import { Conversion } from '../conversion/conversion';
+import { ConversionData } from '../../types';
 
 export const Converter = () => {
   const { isLoading, data } = useExchangeRatesQuery();
   const { t } = useTranslation();
-  const [conversionData, setConversionData] = useState<ConversionData | null>(
-    null
-  );
+  const [conversionData, setConversionData] = useState<ConversionData | null>(null);
 
   const handleConversion = (data: ConversionData) => {
     setConversionData(data);
   };
 
   if (isLoading) {
-    return <div>{t("loading")}</div>;
+    return <div>{t('loading')}</div>;
   }
 
   if (!data) {
-    return null;
+    return <Alert severity="error">{t('error')}</Alert>;
   }
 
   const { date, rates, header } = data;
@@ -33,34 +31,27 @@ export const Converter = () => {
     <>
       <Container maxWidth="sm" sx={{ pt: 2 }}>
         <Typography variant="h1" sx={{ mb: 2 }}>
-          {t("title")}
+          {t('title')}
         </Typography>
       </Container>
 
       <Container maxWidth="sm" sx={{ mb: 4 }}>
-        <Typography>{t("info.disclaimer")}</Typography>
-        <Typography>
-          {t("info.updatedAt", { date: moment(date).format("DD.MM.YYYY") })}
-        </Typography>
+        <Typography>{t('info.disclaimer')}</Typography>
+        <Typography>{t('info.updatedAt', { date: moment(date).format('DD.MM.YYYY') })}</Typography>
       </Container>
 
       <Container maxWidth="sm" sx={{ mb: 4 }}>
         <Card>
           <CardContent>
             <Form rates={rates} handleConversion={handleConversion} />
-            {conversionData && (
-              <Conversion
-                rate={conversionData.rate}
-                amount={conversionData.amount}
-              />
-            )}
+            {conversionData && <Conversion rate={conversionData.rate} amount={conversionData.amount} />}
           </CardContent>
         </Card>
       </Container>
 
       <Container maxWidth="sm" sx={{ mb: 4 }}>
         <Typography variant="h2" sx={{ mb: 2 }}>
-          {t("exchangeRates.title")}
+          {t('exchangeRates.title')}
         </Typography>
         <Card>
           <CardContent>
--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,5 +1,5 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -7,21 +7,21 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   en: {
     translation: {
-      title: "Currency converter",
-      loading: "Loading...",
+      title: 'Currency converter',
+      loading: 'Loading...',
+      error: 'Application error - server is not responding.',
       info: {
-        disclaimer:
-          "Foreign exchange market rates are updated only once a day.",
-        updatedAt: "Last update: {{date}}",
+        disclaimer: 'Foreign exchange market rates are updated only once a day.',
+        updatedAt: 'Last update: {{date}}',
       },
       exchangeRates: {
-        title: "Exchange rates",
+        title: 'Exchange rates',
       },
       form: {
-        amount: "Amount CZK",
-        code: "Select currency",
-        submit: "Convert"
-      }
+        amount: 'Amount CZK',
+        code: 'Select currency',
+        submit: 'Convert',
+      },
     },
   },
 };
@@ -30,7 +30,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
